613a9851e8b6f83eb938589367da0f35df0861f2
Gamaliel Padillo <gamalielgad1234@gmail.com>
2017-05-31 08:53:19 -0700

    First commit

---
+++ tic_tac_toe_cpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  tic_tac_toe_cpp
+//
+//  Created by Gamaliel Padillo on 5/31/17.
+//  Copyright © 2017 Gamaliel Padillo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
